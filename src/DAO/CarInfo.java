6942683b689dbcd4ad10b608763775fc839a87cc
djjkobe <djjkobe@gmail.com>
2015-03-30 13:52:39 -0400

    fixed coding style

--- src/DAO/CarInfo.java
+++ src/DAO/CarInfo.java
@@ -1,6 +1,7 @@
 package DAO;
 
 public class CarInfo {
+
 	private double price;
 	private String engine;
 	private double mileage;
@@ -12,69 +13,93 @@ public class CarInfo {
 	private String bodyStyle;
 	private String transmission;
 	private String image;
+
+
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public String getEngine() {
 		return engine;
 	}
+
 	public void setEngine(String engine) {
 		this.engine = engine;
 	}
+
 	public double getMileage() {
 		return mileage;
 	}
+
 	public void setMileage(double mileage) {
 		this.mileage = mileage;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getYear() {
 		return year;
 	}
+
 	public void setYear(int year) {
 		this.year = year;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
+
 	public int getDoorCount() {
 		return doorCount;
 	}
+
 	public void setDoorCount(int doorCount) {
 		this.doorCount = doorCount;
 	}
+
 	public String getDriveTrain() {
 		return driveTrain;
 	}
+
 	public void setDriveTrain(String driveTrain) {
 		this.driveTrain = driveTrain;
 	}
+
 	public String getBodyStyle() {
 		return bodyStyle;
 	}
+
 	public void setBodyStyle(String bodyStyle) {
 		this.bodyStyle = bodyStyle;
 	}
+
 	public String getTransmission() {
 		return transmission;
 	}
+
 	public void setTransmission(String transmission) {
 		this.transmission = transmission;
 	}
+
 	public String getImage() {
 		return image;
 	}
+	
 	public void setImage(String image) {
 		this.image = image;
 	}
--- src/project/Median.java
+++ src/project/Median.java
@@ -2,6 +2,7 @@ package project;
 
 public class Median {
 	public static void main(String []args){
+
 		int[] A= new int[6];
 		int[] B = new int[4];
 		for (int i = 0; i < 6; i++){
@@ -16,54 +17,57 @@ public class Median {
 	 System.out.println("median is "+m.findMedianSortedArrays(A, B));
 		
 	}
-	    public double findMedianSortedArrays(int[] A, int[] B) {
+	
+	public double findMedianSortedArrays(int[] A, int[] B) {
 	        // write your code here
-	        if (A == null || A.length == 0 || B == null || B.length == 0){
-	            return -1.0;
-	        }
 	        
-	        int m = A.length;
-	        int n = B.length;
-	        int index = m + n;
-	        int[] C = new int[index];
-	        int count = 0;
-	        int i = 0;
-	        int j = 0;
-	         while (i + j < m + n - 1){
-	            if (i == m - 1){
-	                C[i + j] = B[j];
-	                j++;
-	            }
-	            else if (j == n - 1){
-	                C[i + j] = A[i];
+		if (A == null || A.length == 0 || B == null || B.length == 0){
+	            return -1.0;
+	  	}
+	            
+	  	int m = A.length;
+	    int n = B.length;
+	    int index = m + n;
+	    int[] C = new int[index];
+	    int count = 0;
+	    int i = 0;
+	    int j = 0;
+	    while (i + j < m + n - 1){
+	        if (i == m - 1){
+	            C[i + j] = B[j];
+	            j++;
+	        }
+	        else if (j == n - 1){
+	            C[i + j] = A[i];
+	            i++;
+	        }
+	        else {
+	          	if (A[i] <= B[j]){
+	                C[i + j] = A[i] ;
 	                i++;
 	            }
 	            else {
-	                if (A[i] <= B[j]){
-	                    C[i + j] = A[i] ;
-	                    i++;
-	                }
-	                else {
-	                    C[i + j] = B[j] ;
-	                    j++;
-	                }
+	                C[i + j] = B[j] ;
+	                j++;
 	            }
-	            
 	        }
-	         for (int w = 0; w < 10; w++){
-	 			
-	 			System.out.println(C[w]);
-	 		}
-	       System.out.println("C length is "+C.length);
-	       System.out.println("C 10 is "+C[9]);
-	        if ((m + n) % 2 == 0){
-	        	System.out.println("aaa "+ C[index/2] +" bbb "+ C[index/2 - 1]);
-	            return (double)(C[index/2] + C[index/2 - 1])/2;
-	        }
-	        else {
-	            return (double)(C[index/2]);
-	        }
+	           
 	    }
+
+	    for (int w = 0; w < 10; w++){
+	 			
+	 		System.out.println(C[w]);
+	 	}
+	  	System.out.println("C length is "+C.length);
+	   	System.out.println("C 10 is "+C[9]);
+	   	if ((m + n) % 2 == 0){
+	     	System.out.println("aaa "+ C[index/2] +" bbb "+ C[index/2 - 1]);
+	      	return (double)(C[index/2] + C[index/2 - 1])/2;
+	   	}
+	    else {
+	     	return (double)(C[index/2]);
+	    }
+	}
 
 
 
